40ef735c2762429eac7b84c2713b34a44ebfc24f
Sheikh Sabbir Ahmad <22234103362@cse.bubt.edu.bd>
2026-05-10 13:29:21 +0600

    Fix mobile responsive bugs

--- src/components/PopularProducts.jsx
+++ src/components/PopularProducts.jsx
@@ -11,11 +11,12 @@ const PopularProducts = async () => {
   const popularProducts = products.slice(0, 3);
 
   return (
-    <div className="mt-15">
-      <h1 className="text-3xl font-bold my-5 text-[#FF7A18]">Popular Products</h1>
-      
+    <div className="mt-15 px-4 sm:px-6 lg:px-0">
+      <h1 className="text-2xl sm:text-3xl font-bold my-5 text-[#FF7A18] text-center sm:text-left">
+        Popular Products
+      </h1>
 
-      <div className="grid grid-cols-3 gap-5">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
         {popularProducts.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
